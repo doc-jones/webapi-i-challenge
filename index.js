71b983c6d803505a04be38bdfeb77615400dd92d
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-27 18:21:11 -0400

    Add get request handler

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+// express = lightweight
+// routers -> organizing our endpoints
+// middleware -> allows us to expand & customize
 
 const server = express();
 
+// creating endpoints
+server.get('/', (req, res) => {
+    console.log('inside the get request');
+    // specify data type
+    // set a status code
+    // send a response
+    res.send('<h2>Hello World</h2>');
+});
+
+server.get('/now', (req, res) => {
+    const now = new Date().toISOString();
+    res.send(now);
+});
 
 
 
+// listening
 server.listen(5050, () => {
     console.log('Listening on port 5050');
 });
